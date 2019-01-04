0b795298bf5bf38fe853bf277c022d72928493c4
Jurij Podgorsek <podgorsek.jurij@gmail.com>
2019-01-04 23:22:25 +0100

    Fix poskus 2

--- keyboards/handwired/daktil/keymaps/default/keymap.c
+++ keyboards/handwired/daktil/keymaps/default/keymap.c
@@ -41,18 +41,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(SYMB,KC_GRV),         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
           KC_LSPO,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,
           KC_TRNS,      KC_TRNS,  LALT(KC_LSFT),   KC_INS,  KC_TRNS, KC_TRNS,
-                                                    KC_SPC, KC_BSPC,
+           KC_BSPC, KC_SPC,
                                                             LGUI_T(KC_ESC),
-                                            ALT_T(KC_APP), KC_HOME, KC_END,
+           KC_HOME, ALT_T(KC_APP), KC_END,
         // right hand
                              KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
                              KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
                              KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
                              KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSPC,
                                   KC_TRNS,  KC_LBRC,  KC_RBRC,            KC_TRNS,         KC_TRNS,
-                     KC_TAB, KC_ENT,
+           KC_ENT,KC_TAB,
     LGUI_T(KC_ESC),
-           KC_PGUP, KC_PGDN, KC_RALT
+           KC_RALT, KC_PGDN, KC_PGUP,
     ),
 /* Keymap 1: Symbol Layer
  *
