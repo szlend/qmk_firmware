ba0d7dd2d5fc26cfba4710a9d326879f22f83b94
Jurij Podgoršek <podgorsek.jurij@gmail.com>
2019-03-05 00:35:14 +0100

    Še več popravkov

--- keyboards/handwired/daktil/keymaps/default/keymap.c
+++ keyboards/handwired/daktil/keymaps/default/keymap.c
@@ -42,11 +42,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LT(SYMB,KC_GRV),  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,
           KC_TRNS,      KC_TRNS,  LALT(KC_LSFT),   KC_INS,  KC_TRNS,
 
-                                                            KC_BSPC,
+                                                  KC_BSPC,
                                                    KC_SPC,
-                                                                    LGUI_T(KC_ESC),
-                                            ALT_T(KC_APP),                 KC_HOME,
-                                                             KC_END,
+                                           LGUI_T(KC_ESC),
+           KC_HOME,
+           KC_END,
+                                            ALT_T(KC_APP),
         // right hand
                              KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
                              KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
@@ -66,14 +67,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |Versn |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   !  |   @  |   {  |   }  |   |  |                    |  Up  |   7  |   8  |   9  |   *  |  F12 |
+ * |      |   !  |   @  |   {  |   }  |   |  |                    |   &  |   7  |   8  |   9  |   *  |  F12 |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   #  |   $  |   (  |   )  |   `  |                    | Down |   4  |   5  |   6  |   +  |      |
+ * |      |   #  |   $  |   (  |   )  |   `  |                    |   ^  |   4  |   5  |   6  |   +  |   =  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |      |   %  |   ^  |   [  |   ]  |   ~  |                    |   &  |   1  |   2  |   3  |   -  |      |
+ * |      |   %  |   ^  |   [  |   ]  |   ~  |                    |   _  |   1  |   2  |   3  |   -  |      |
  * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
- * |RESET |      |      |      |      |                                  |      |   .  |   0  |   =  |      |
- * `----------------------------------'                                  `----------------------------------'
+ * |RESET |      |      |      |      |                                  |      |   .  |   0  |
+ * `----------------------------------'                                  `--------------------'
  *                                      ,-------------.  ,-------------.
  *                                      |      |      |  |      |      |
  *                               ,------|------|------|  |------+------+------.
@@ -86,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = LAYOUT_dactyl(
        // left hand
        KC_TRNS,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
-          VRSN,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,
+          VRSN,  KC_EXLM,    KC_AT,  KC_HASH,   KC_DLR,  KC_PERC,
        KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,
        KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
          RESET,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -95,9 +96,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS,  KC_TRNS,  KC_TRNS,
        // right hand
                 KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
-                KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
-                KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
-                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_MINS,  KC_TRNS,
+                KC_AMPR,     KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
+                KC_CIRC,     KC_4,    KC_5,  KC_6, KC_PLUS,   KC_EQL,
+                KC_UNDS,     KC_1,    KC_2,  KC_3, KC_MINS,  KC_TRNS,
                           KC_TRNS,  KC_DOT,  KC_0,  KC_EQL,  KC_TRNS,
       KC_TRNS,  KC_TRNS,
       KC_TRNS,
