56d8efc06195817786801dd236292fadd6d6314d
Jurij Podgoršek <podgorsek.jurij@gmail.com>
2019-04-01 22:34:02 +0200

    Properly popravljen thumb cluster

--- keyboards/handwired/dactyl/dactyl.h
+++ keyboards/handwired/dactyl/dactyl.h
@@ -39,10 +39,9 @@ void init_expander(void);
     k10,k11,k12,k13,k14,k15,                                            \
     k20,k21,k22,k23,k24,k25,                                            \
     k30,k31,k32,k33,k34,k35,                                            \
-    k40,k41,k42,k43,k44,                                                \
-                            k55,k50,                                    \
-                                k54,                                    \
-                        k53,k52,k51,                                    \
+            k42,k43,k44,k45,                                            \
+                    k52,k53,                                            \
+                    k54,k55,                                            \
                                                                         \
             k06,k07,k08,k09,k0A,k0B,                                    \
             k16,k17,k18,k19,k1A,k1B,                                    \
@@ -59,8 +58,8 @@ void init_expander(void);
     { k10, k11, k12, k13, k14, k15,     k16, k17, k18, k19, k1A, k1B }, \
     { k20, k21, k22, k23, k24, k25,     k26, k27, k28, k29, k2A, k2B }, \
     { k30, k31, k32, k33, k34, k35,     k36, k37, k38, k39, k3A, k3B }, \
-    { k40, k41, k42, k43, k44, KC_NO, KC_NO, k47, k48, k49, k4A, k4B }, \
-    { k50, k51, k52, k53, k54, k55,     k56, k57, k58, k59, k5A, k5B }, \
+   {KC_NO,KC_NO,k42, k43, k44, k45,   KC_NO, k47, k48, k49, k4A, k4B }, \
+   {KC_NO,KC_NO,k52, k53, k54, k55,     k56, k57, k58, k59, k5A, k5B }, \
    }
 
 
