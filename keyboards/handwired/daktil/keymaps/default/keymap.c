47a5184e4b7ba0b3e3f149330b3d1f7df0bd48b5
Lio Novelli <liopolddoron@gmail.com>
2019-04-16 11:54:02 +0200

    Dynamic macros for old dactyl.

--- keyboards/handwired/daktil/keymaps/default/keymap.c
+++ keyboards/handwired/daktil/keymaps/default/keymap.c
@@ -6,13 +6,17 @@
 #define BASE 0 // default layer
 #define SYMB 1 // symbols
 #define MDIA 2 // media keys
+#define TKTK 3 // tehnoklistir layer
 
 enum custom_keycodes {
   PLACEHOLDER = SAFE_RANGE, // ensure these codes start after the highest keycode defined in Quantum
   VRSN,
   R_ARR,
   R_BARR,
+  DYNAMIC_MACRO_RANGE
 };
+
+#include "dynamic_macro.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
@@ -38,23 +42,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [BASE] = LAYOUT_dactyl(  // layer 0 : default
         // left hand
-           KC_EQL,         KC_1,           KC_2,     KC_3,     KC_4,  KC_5,
+  LT(TKTK, KC_EQL),         KC_1,           KC_2,     KC_3,     KC_4,  KC_5,
           KC_DELT,         KC_Q,           KC_W,     KC_E,     KC_R,  KC_T,
           KC_INS,         KC_A,           KC_S,     KC_D,     KC_F,  KC_G,
           KC_LSPO,  CTL_T(KC_Z),           KC_X,     KC_C,     KC_V,  KC_B,
           LT(SYMB,KC_GRV),ALT_T(KC_QUOT),LALT(KC_LSFT),KC_LEFT,KC_RGHT,
-                                                              LGUI_T(KC_ESC),   KC_SPC,
-                                                                          RALT_T(KC_APP),
-                                                            KC_HOME,    KC_END,  KC_BSPC,
+                                                     LGUI_T(KC_ESC), RALT_T(KC_APP),
+                                                                 KC_HOME,   KC_SPC,
+                                                                   KC_BSPC, KC_END,
         // right hand
-                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         KC_MINS,
+                             KC_6,   KC_7,     KC_8,     KC_9,               KC_0,         LT(TKTK,KC_MINS),
                              KC_Y,   KC_U,     KC_I,     KC_O,               KC_P,         KC_BSLS,
                              KC_H,   KC_J,     KC_K,     KC_L,  LT(MDIA, KC_SCLN),  GUI_T(KC_QUOT),
                              KC_N,   KC_M,  KC_COMM,   KC_DOT,     CTL_T(KC_SLSH),         KC_RSPC,
                              KC_DOWN,  KC_UP,  KC_LBRC,      ALT_T(KC_RBRC),          KC_FN1,
-             KC_ENT, RGUI_T(KC_ESC),
-             RALT_T(KC_APP),
-             KC_TAB, KC_PGDN, KC_PGUP
+           RALT_T(KC_APP),  RGUI_T(KC_ESC),
+           KC_PGUP, KC_PGDN,
+           KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -83,20 +87,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
           VRSN,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
        KC_TRNS,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,
        KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,
-       KC_TRNS,  KC_PERC,  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
-         RESET,  R_ARR,     R_BARR,  KC_LT,  KC_GT,
-                                                         KC_TRNS,  KC_ENT,
-                                                                   KC_TRNS,
-                                               KC_EQL,  KC_PLUS,  KC_TRNS,
+          KC_TRNS, CTL_T(KC_PERC),  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
+          RESET,            R_ARR,   R_BARR,     KC_LT,  KC_GT,
+                                                         KC_TRNS,  KC_TRNS,
+                                                                   KC_EQL,
+                                                KC_ENT,  KC_TRNS,  KC_PLUS,
        // right hand
                 KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
                 KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
                 KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
-                KC_AMPR,     KC_1,    KC_2,  KC_3, KC_BSLS,  KC_TRNS,
-                          KC_MINS,  KC_DOT,  KC_0,  KC_EQL,  KC_TRNS,
-      KC_SPC,  KC_TRNS,
-      KC_TRNS,
-      KC_TRNS,  KC_UNDS,  KC_MINS
+                KC_AMPR,     KC_1,    KC_2,  KC_3, CTL_T(KC_BSLS),  KC_TRNS,
+                KC_MINS,  KC_DOT,  KC_0,  ALT_T(KC_EQL),  KC_TRNS,
+      KC_TRNS,  KC_TRNS,
+      KC_MINS,
+      KC_UNDS,  KC_TRNS,  KC_SPC
 ),
 /* Keymap 2: Media and mouse keys
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -126,9 +130,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,  KC_VOLU,  KC_MS_L,  KC_MS_D,  KC_MS_R,  KC_TRNS,
        KC_TRNS,  KC_VOLD,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
        KC_TRNS,  KC_MUTE,  KC_TRNS,  KC_BTN1,  KC_BTN2,
-                                                         KC_TRNS,  KC_BTN1,
-                                                                   KC_TRNS,
-                                               KC_WH_U,  KC_WH_D,  KC_BTN2,
+                                                         KC_TRNS,  KC_TRNS,
+                                                                   KC_WH_U,
+                                                KC_BTN1,  KC_BTN2, KC_WH_D,
     // right hand
                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
                 KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
@@ -138,6 +142,47 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_WBAK,  KC_TRNS,
       KC_TRNS,
       KC_TRNS,  KC_TRNS,  KC_WBAK
+),
+/* Keymap 3: Tehnoklistir, dynamic macros actually
+ *
+ * ,-----------------------------------------.                    ,-----------------------------------------.
+ * |Versn |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  F11 |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   !  |   @  |   {  |   }  |   |  |                    |  Up  |   7  |   8  |   9  |   *  |  F12 |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   #  |   $  |   (  |   )  |   `  |                    | Down |   4  |   5  |   6  |   +  |      |
+ * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
+ * |      |   %  |   ^  |   [  |   ]  |   ~  |                    |   &  |   1  |   2  |   3  |   \  |      |
+ * |------+------+------+------+------+------'                    `------+------+------+------+------+------|
+ * |RESET |  ->  |  =>  |   <  |   >  |                                  |   -  |   .  |   0  |   =  |      |
+ * `----------------------------------'                                  `----------------------------------'
+ *                                      ,-------------.  ,-------------.
+ *                                      | Stop |      |  |      | Stop |
+ *                               ,------|------|------|  |------+------+------.
+ *                               |      |      |   =  |  |  -   |      |      |
+ *                               |Macro1|Start1|------|  |------|Start2|Macro2|
+ *                               |      |      |   +  |  |  _   |      |      |
+ *                               `--------------------'  `--------------------'
+ */
+// Tehnoklistir
+[TKTK] = LAYOUT_dactyl(
+       // left hand
+          VRSN,    KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,
+       KC_TRNS,  KC_EXLM,    KC_AT,  KC_LCBR,  KC_RCBR,  KC_PIPE,
+       KC_TRNS,  KC_HASH,   KC_DLR,  KC_LPRN,  KC_RPRN,   KC_GRV,
+          KC_TRNS, CTL_T(KC_PERC),  KC_CIRC,  KC_LBRC,  KC_RBRC,  KC_TILD,
+         RESET,  R_ARR,     R_BARR,  KC_LT,  KC_GT,
+                                                   DYN_REC_STOP, KC_TRNS,
+                                                                 KC_EQL,
+                              DYN_MACRO_PLAY1,  DYN_REC_START1,  KC_PLUS,
+       // right hand
+                KC_F6,      KC_F7,   KC_F8, KC_F9,  KC_F10,   KC_F11,
+                KC_UP,       KC_7,    KC_8,  KC_9, KC_ASTR,   KC_F12,
+                KC_DOWN,     KC_4,    KC_5,  KC_6, KC_PLUS,  KC_TRNS,
+                KC_AMPR,     KC_1,    KC_2,  KC_3, CTL_T(KC_BSLS),  KC_TRNS,
+                KC_MINS,  KC_DOT,  KC_0,  ALT_T(KC_EQL),  KC_TRNS,
+          KC_TRNS,       DYN_REC_STOP,
+          KC_UNDS,  KC_MINS, DYN_REC_START2, DYN_MACRO_PLAY2
 ),
 };
 
@@ -164,6 +209,9 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  if (!process_record_dynamic_macro(keycode, record)) {
+    return false;
+  }
   switch (keycode) {
     case VRSN:
       if (record->event.pressed) {
